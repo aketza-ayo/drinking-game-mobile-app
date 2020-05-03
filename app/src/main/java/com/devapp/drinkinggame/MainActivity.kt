939ea038e7b64d8737e06bb6e7935713b02d9ad9
Aketza Ayo <aketzamaster@gmail.com>
2020-05-03 13:18:24 +0200

    - Adding the actions for the toolbar three dots in MainActivity

--- app/src/main/java/com/devapp/drinkinggame/MainActivity.kt
+++ app/src/main/java/com/devapp/drinkinggame/MainActivity.kt
@@ -3,6 +3,8 @@ package com.devapp.drinkinggame
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log.d
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,9 +18,26 @@ class MainActivity : AppCompatActivity() {
     var mutableDeck = CardsData().getAllCards()
     private  var showLogOnlyOnce = true
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         viewDeck.setOnClickListener {
